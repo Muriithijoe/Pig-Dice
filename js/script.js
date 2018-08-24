5b365f5812b6bbf6ffd455db16c5924f25ca038c
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-08-24 22:43:19 +0300

    business logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 //business logic
+$(document).ready(function(){
+  $("#startgame").click(function(){
+  $("#gamestart").show();
+});
+});
+
+$(document).ready(function(){
+  $("#newgame").click(function(){
+  $("#gamestart").hide();
+});
+});
+
+//user interface
